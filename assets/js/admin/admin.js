05a71bf16bded9cd962fe2d6afed5d41c45f10da
Trong-123 <trongngo.02092004@gmail.com>
2025-02-11 13:55:10 +0700

    commit

--- assets/js/admin/admin.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('openAuthorModal').addEventListener('click', function (e) {
-    const AuthorModal = new bootstrap.Modal(document.getElementById('AuthorModal'));
-    AuthorModal.show(); // Hiển thị modal chọn tác giả
-});
