ae3b7a90f626f6151fab010f103ab30777f8d2ee
Trong-123 <trongngo.02092004@gmail.com>
2025-02-15 09:32:19 +0700

    commit

--- assets/js/admin/chartTop.js
+++ assets/js/admin/chartTop.js
@@ -3,7 +3,7 @@ const data = {
     labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
     datasets: [
       {
-        label: 'Dataset 1',
+        label: 'Số lượng: ',
         data: [12, 14, 25, 6, 7],
         // backgroundColor: ['rgba(123, 233, 145, 0.3','rgba(103, 238, 145, 0.3','rgba(52, 223, 145, 0.3','rgba(33, 213, 145, 0.3','rgba(79, 243, 145, 0.3']
       }
